6991319450d6c7f78208e97edf241aa810c2bbb2
Patrick Lamm <lammster@gmail.com>
2021-08-14 19:49:06 -0400

    Dependency Injection updated

--- lib/TransDev.Invoicing.Application/DependencyInjection.cs
+++ lib/TransDev.Invoicing.Application/DependencyInjection.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +12,12 @@ namespace TransDev.Invoicing.Application
 {
     public static class DependencyInjection
     {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
     }
 }
--- lib/TransDev.Invoicing.Infrastructure/DependencyInjection.cs
+++ lib/TransDev.Invoicing.Infrastructure/DependencyInjection.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TransDev.Invoicing.Application.Common.Interfaces;
+using TransDev.Invoicing.Infrastructure.Persistance;
 
 namespace TransDev.Invoicing.Infrastructure
 {
     public static class DependencyInjection
     {
+        public static IServiceCollection AddInfrastructre(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (configuration.GetValue<bool>("UseInMemoryDatabase") || configuration["inmemorydb"] == "true")
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options
+                        .UseLoggerFactory(MyLoggerFactory)
+                        .EnableSensitiveDataLogging(true)
+                        .UseInMemoryDatabase("cleanDb"));
+            }
+            else
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options
+                        .UseLoggerFactory(MyLoggerFactory)
+                        .EnableSensitiveDataLogging(true)
+                        .UseSqlServer(
+                        configuration.GetConnectionString("DefaultConnection"),
+                        b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+            }
+
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
+
+            return services;
+        }
+        public static readonly ILoggerFactory MyLoggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole();
+        });
     }
 }
