a44e6823ec4d82b2928761bc91ba46a6def989ce
Patrick Lamm <lammster@gmail.com>
2021-08-25 14:19:40 -0400

    check point

--- web/TransDev.Invoicing.WebUI/ClientApp/src/App.vue
+++ web/TransDev.Invoicing.WebUI/ClientApp/src/App.vue
@@ -1,20 +1,32 @@
 <template>
   <v-app>
+    <v-navigation-drawer v-model="drawer"
+                         app>
+      <nav-list></nav-list>
+    </v-navigation-drawer>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Simple Invoicing</v-toolbar-title>
+    </v-app-bar>
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import NavList from './components/NavList.vue'
 
 export default defineComponent({
   name: 'App',
-
+  components: {
+    NavList
+  },
   data () {
     return {
-      //
+      drawer: null
     }
   }
 })
