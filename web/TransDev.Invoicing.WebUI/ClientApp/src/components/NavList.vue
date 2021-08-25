71b056a4b4e217e40de82f0e07ab0738811fc103
Patrick Lamm <lammster@gmail.com>
2021-08-25 17:36:24 -0400

    updating NavList

--- web/TransDev.Invoicing.WebUI/ClientApp/src/components/NavList.vue
+++ web/TransDev.Invoicing.WebUI/ClientApp/src/components/NavList.vue
@@ -1,25 +1,27 @@
 <template>
-<v-card class="mx-auto"
-        max-width="300"
-        tile>
-  <v-list rounded>
-    <v-subheader>REPORTS</v-subheader>
-    <v-list-item-group v-model="selectedItem"
-                        color="primary">
-      <v-list-item v-for="(item, i) in items"
-                    :key="i">
-        <v-list-item-icon>
-          <v-icon v-text="item.icon"></v-icon>
-        </v-list-item-icon>
-        <v-list-item-content>
-          <v-list-item-title v-text="item.text"></v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list-item-group>
-  </v-list>
-</v-card>
+  <v-card class="mx-auto"
+          max-width="300"
+          tile>
+    <v-list rounded>
+      <v-subheader>REPORTS</v-subheader>
+      <v-list-item-group v-model="selectedItem"
+                         color="primary">
+        <v-list-item v-for="(item, i) in items"
+                     :key="i">
+          <!--<v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>-->
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+            {{ item.icon }}
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
 </template>
-<script>
+
+<script lang="ts">
 export default {
   data: () => ({
     selectedItem: 1,
