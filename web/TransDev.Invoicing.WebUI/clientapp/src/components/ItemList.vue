6537541d777240cfca587b8ab19eaf5913a401e4
Patrick Lamm <lammster@gmail.com>
2021-08-30 15:12:18 -0400

    check in

--- web/TransDev.Invoicing.WebUI/clientapp/src/components/ItemList.vue
+++ web/TransDev.Invoicing.WebUI/clientapp/src/components/ItemList.vue
@@ -12,30 +12,34 @@
   </v-container>
 </template>
 
-<script>
-import onMounted from 'vue'
+<script lang="ts">
+import { defineComponent } from 'vue'
 import { ItemClient, IItemDto, GetActiveItemsQuery } from '../models/serviceModels'
 
-export default {
-  name: 'Item List',
-  data: () => ({
-    items: []
-  }),
-  setup () {
-    const client = new ItemClient()
-    const initQuery = new GetActiveItemsQuery()
-    initQuery.page = 1
-    initQuery.pageSize = 25
-    initQuery.searchQuery = ''
-    loadItems([])
-    // console.log(this.$data)
-    // client.searchActiveItems(initQuery).then(results => { console.log('test') })
+export default defineComponent({
+  name: 'ItemList',
+  data: () => {
+    return {
+      items: []
+    }
+  },
+  mounted () {
+    initializeData()
   },
   methods: {
+    initalizeData: function () {
+      const client = new ItemClient()
+      const initQuery = new GetActiveItemsQuery()
+      initQuery.page = 1
+      initQuery.pageSize = 25
+      initQuery.searchQuery = ''
+      client.searchActiveItems(initQuery).then(results => console.log(results.items))
+    },
     loadItems: function (items) {
       // this.data.items = items
       console.log('loadItems')
     }
   }
-}
+})
+
 </script>
