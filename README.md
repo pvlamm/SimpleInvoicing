018c94da790515ccb04d8ac56cac22446d08d7ec
Patrick Lamm <lammster@gmail.com>
2021-09-01 10:13:22 -0400

    Mild document update

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# SimpleInvoicing
-A simple invoicing system and generates a basic report to send out to clients
+# Simple Invoicing
+
+Intended for small scale service workers looking to upgrade their Invoicing practices from something simple like Excel or Google-Documents to something more standardized
+
+# Third Party Libraries / Technologies
+
+## Backend and Database Interaction
+
+1. SQL Server
+1. EntityFramework Core
+
+## Middleware
+
+1. AutoMapper
+1. FluentValidation
+1. MediatR
+1. Newtonsoft.Json 
+1. NSwag 
+1. Swashbuckle 
+1. VueCliMiddleware
+
+## Front End 
+
+1. VueJS
+1. Vuetify
+1. Vuex
--- lib/TransDev.Invoicing.Application/Authentication/Queries/AuthenticateUser/AuthenticateUserQuery.cs
+++ lib/TransDev.Invoicing.Application/Authentication/Queries/AuthenticateUser/AuthenticateUserQuery.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using TransDev.Invoicing.Application.Common.Interfaces;
 
 namespace TransDev.Invoicing.Application.Authentication.Queries.AuthenticateUser
 {
@@ -16,9 +17,21 @@ namespace TransDev.Invoicing.Application.Authentication.Queries.AuthenticateUser
 
     public class AuthenticateUserQueryHandler : IRequestHandler<AuthenticateUserQuery, AuthenticateUserResponse>
     {
-        public Task<AuthenticateUserResponse> Handle(AuthenticateUserQuery request, CancellationToken cancellationToken)
+        private readonly IAuthenticationService _service;
+
+        public AuthenticateUserQueryHandler(IAuthenticationService service)
         {
-            throw new NotImplementedException();
+            _service = service;
+        }
+
+        public async Task<AuthenticateUserResponse> Handle(AuthenticateUserQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _service.AuthenticateAsync(request.Username, request.Password);
+
+            return new AuthenticateUserResponse
+            {
+                IsSuccess = result
+            };
         }
     }
 }
