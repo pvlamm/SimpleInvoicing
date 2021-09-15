481fb9b68be717c527882926bb059d4ed64f8e23
Patrick Lamm <lammster@gmail.com>
2021-09-15 16:59:42 -0400

    MSAL improvements

--- web/TransDev.Invoicing.WebUI/clientapp/src/views/Login.vue
+++ web/TransDev.Invoicing.WebUI/clientapp/src/views/Login.vue
@@ -6,72 +6,29 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import LoginForm from '../components/LoginForm.vue'
-import { AuthenticationClient, AuthenticateUserQuery } from '../models/serviceModels'
-import { PublicClientApplication, InteractionType } from '@azure/msal-browser';
-import { MsalInterceptor, MsalModule } from '@azure/msal-angular';
+import * as Msal from 'msal'
+// if using cdn version, 'Msal' will be available in the global scope
 
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
-  //"AzureAd": {
-  //  "Instance": "https://localhost:5001/",
-  //    "Domain": "qualified.domain.name",
-  //      "TenantId": "ff31a505-fd8f-4e72-a4d5-cd61b9cc079d",
-  //        "ClientId": "b4dafc40-8606-497e-9880-7364993da30d",
-
-  //          "CallbackPath": "/signin-oidc"
-  //}
 const msalConfig = {
   auth: {
-    clientId: "b4dafc40-8606-497e-9880-7364993da30d",
-    authority: "Enter_the_Cloud_Instance_Id_HereEnter_the_Tenant_Info_Here",
-    redirectUri: "https://localhost:5001/",
-  },
-  cache: {
-    cacheLocation: "sessionStorage", // This configures where your cache will be stored
-    storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    clientId: 'b4dafc40-8606-497e-9880-7364993da30d'
   }
-};
+}
 
-// Add here scopes for id token to be used at MS Identity Platform endpoints.
-const loginRequest = {
-  scopes: ["openid", "profile", "User.Read"]
-};
+const msalInstance = new Msal.UserAgentApplication(msalConfig)
 
-// Add here scopes for access token to be used at MS Graph API endpoints.
-const tokenRequest = {
-  scopes: ["Mail.Read"]
-};
+var loginRequest = {
+  scopes: ['user.read', 'mail.send'] // optional Array<string>
+}
 
-export default defineComponent({
-  name: 'Login',
-  data: function () {
-    return {
-      login: {} as AuthenticateUserQuery | null
-    }
-  },
-  components: {
-    LoginForm
-  },
-  mounted: function () {
-    this.login = new AuthenticateUserQuery()
-    this.login.username = 'test'
-    this.login.password = 'testtest'
-  },
-  methods: {
-    async authenticate (tempLogin: AuthenticateUserQuery) {
-      this.$data.login = tempLogin
-      console.log(tempLogin.username, tempLogin.password)
-      const client = new AuthenticationClient()
-      const query = new AuthenticateUserQuery()
-      query.username = tempLogin.username
-      query.password = tempLogin.password
-      const result = await client.authenticate(query).then(result => console.log(result)).catch(error => console.log(error))
-      console.log(this.$data.login)
-      return true
-    }
-  }
-})
+msalInstance.loginPopup(loginRequest)
+  .then(response => {
+    // handle response
+    console.log(response)
+  })
+  .catch(err => {
+    // handle error
+    console.log(err)
+  })
+
 </script>
