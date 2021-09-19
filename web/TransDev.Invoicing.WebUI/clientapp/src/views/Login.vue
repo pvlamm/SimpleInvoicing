a52c4cc1777b2b5f21464bdb5c289252da210164
Patrick Lamm <lammster@gmail.com>
2021-09-19 15:05:10 -0400

    Additional Improvements in figuring out the msalConfig

--- web/TransDev.Invoicing.WebUI/clientapp/src/views/Login.vue
+++ web/TransDev.Invoicing.WebUI/clientapp/src/views/Login.vue
@@ -5,17 +5,28 @@
 </template>
 
 <script lang="ts">
-import * as Msal from 'msal'
+import * as Msal from '@azure/msal-browser'
 // if using cdn version, 'Msal' will be available in the global scope
 
 const msalConfig = {
   auth: {
     clientId: 'b4dafc40-8606-497e-9880-7364993da30d',
-    authority: 'https://login.microsoftonline.com/common'
+    authority: 'https://login.microsoftonline.com/ff31a505-fd8f-4e72-a4d5-cd61b9cc079d/oauth2/v2.0/authorize'
+  },
+  cache: {
+    storeAuthStateInCookie: false
+  },
+  system: {
+    loggerOptions: {
+      piiLoggingEnabled: false
+    },
+    windowHashTimeout: 60000,
+    iframeHashTimeout: 6000,
+    loadFrameTimeout: 0
   }
 }
 
-const msalInstance = new Msal.UserAgentApplication(msalConfig)
+const msalInstance = new Msal.PublicClientApplication(msalConfig)
 
 var loginRequest = {
   scopes: ['user.read', 'mail.send'] // optional Array<string>
