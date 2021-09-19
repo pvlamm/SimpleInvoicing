1a6362d0b1a6b0ce1079b76574eac90ff8d3dbfb
Patrick Lamm <lammster@gmail.com>
2021-09-19 15:24:49 -0400

    fixed signature

--- lib/TransDev.Invoicing.Application/Common/Interfaces/IInvoiceService.cs
+++ lib/TransDev.Invoicing.Application/Common/Interfaces/IInvoiceService.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TransDev.Invoicing.Domain.Entities;
 
 namespace TransDev.Invoicing.Application.Common.Interfaces
 {
     public interface IInvoiceService
     {
-        Task<Invoice>
+        Task<Invoice> GetInvoiceByPublicId(Guid publicId);
     }
 }
