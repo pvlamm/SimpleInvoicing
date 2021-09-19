34629c508a1fd7cefb705ec2c1570f77e688f82d
Patrick Lamm <lammster@gmail.com>
2021-09-19 15:24:07 -0400

    Creating Initial Invoice Outlines

---
+++ lib/TransDev.Invoicing.Application/Common/Dtos/InvoiceDetailDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransDev.Invoicing.Application.Common.Dtos
+{
+    public class InvoiceDetailDto
+    {
+        public Guid PublicId { get; set; }
+        public ushort SequenceNumber { get; set; }
+        public Guid ItemPublidId { get; set; }
+        public string ItemCode { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal Quantity { get; set; }
+
+    }
+}
---
+++ lib/TransDev.Invoicing.Application/Invoices/Queries/GetInvoiceById/GetInvoiceByIdQuery.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TransDev.Invoicing.Application.Invoices.Queries.GetInvoiceById
+{
+    public class GetInvoiceByIdQuery : IRequest<GetInvoiceByIdResponse>
+    {
+        public Guid PublicId { get; set; }
+    }
+
+    public class GetInvoiceByIdQueryHandler : IRequestHandler<GetInvoiceByIdQuery, GetInvoiceByIdResponse>
+    {
+        private readonly IMapper _mapper;
+
+        public GetInvoiceByIdQueryHandler(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public Task<GetInvoiceByIdResponse> Handle(GetInvoiceByIdQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
