97761f5c80896f1e960f0de80e1dc34e0e14ccfa
Patrick Lamm <lammster@gmail.com>
2021-09-20 13:44:47 -0400

    Updated Login to be within a function

--- web/TransDev.Invoicing.WebUI/clientapp/src/views/Login.vue
+++ web/TransDev.Invoicing.WebUI/clientapp/src/views/Login.vue
@@ -1,6 +1,7 @@
 ﻿<template>
   <v-content>
     <h2>Login Authentication Tester</h2>
+    <button @click="login()">Login</button>
   </v-content>
 </template>
 
@@ -8,41 +9,43 @@
 import * as Msal from '@azure/msal-browser'
 // if using cdn version, 'Msal' will be available in the global scope
 
-const msalConfig = {
-  auth: {
-    clientId: 'b4dafc40-8606-497e-9880-7364993da30d',
-    authority: 'https://login.microsoftonline.com/ff31a505-fd8f-4e72-a4d5-cd61b9cc079d/oauth2/v2.0/authorize'
-  },
-  cache: {
-    storeAuthStateInCookie: false
-  },
-  system: {
-    loggerOptions: {
-      piiLoggingEnabled: false
-    },
-    windowHashTimeout: 60000,
-    iframeHashTimeout: 6000,
-    loadFrameTimeout: 0
-  }
-}
-
-const msalInstance = new Msal.PublicClientApplication(msalConfig)
-
-var loginRequest = {
-  scopes: ['user.read', 'mail.send'] // optional Array<string>
-}
-
-msalInstance.loginPopup(loginRequest)
-  .then(response => {
-    // handle response
-    console.log(response)
-  })
-  .catch(err => {
-    // handle error
-    console.log(err)
-  })
-
 export default {
+  methods: {
+    login () {
+      const msalConfig = {
+        auth: {
+          clientId: 'b4dafc40-8606-497e-9880-7364993da30d'
+        },
+        cache: {
+          cacheLocation: 'sessionStorage',
+          storeAuthStateInCookie: false
+        },
+        system: {
+          loggerOptions: {
+            piiLoggingEnabled: false
+          },
+          windowHashTimeout: 60000,
+          iframeHashTimeout: 6000,
+          loadFrameTimeout: 0
+        }
+      }
 
+      const msalInstance = new Msal.PublicClientApplication(msalConfig)
+
+      var loginRequest = {
+        scopes: ['user.read', 'mail.send'] // optional Array<string>
+      }
+
+      msalInstance.loginPopup(loginRequest)
+        .then(response => {
+          // handle response
+          console.log(response)
+        })
+        .catch(err => {
+          // handle error
+          console.log(err)
+        })
+    }
+  }
 }
 </script>
