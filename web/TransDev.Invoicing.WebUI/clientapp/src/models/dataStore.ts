7275be8c317ff9ee749c74b58ead4ba06071e069
Patrick Lamm <lammster@gmail.com>
2021-09-20 20:54:49 -0400

    extracting dataStore creation to its own file

---
+++ web/TransDev.Invoicing.WebUI/clientapp/src/models/dataStore.ts
@@ -0,0 +1,14 @@
+﻿import { createStore } from 'vuex'
+
+const store = createStore({
+  state() {
+    return {
+      count: 0
+    }
+  },
+  mutations: {
+    increment (state: any) {
+      state.count++
+    }
+  }
+})
