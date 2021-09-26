f9e1b86bdee1945df3e181337bdadd0601740856
Patrick Lamm <lammster@gmail.com>
2021-09-26 15:44:00 -0400

    Working Store State shared across pages
    Working store state shared across pages

--- web/TransDev.Invoicing.WebUI/clientapp/src/views/About.vue
+++ web/TransDev.Invoicing.WebUI/clientapp/src/views/About.vue
@@ -2,7 +2,24 @@
   <div class="about">
     <h1>This is an about page</h1>
     <p>
-      Current Count {{ $store.getters.counter }}
+      Current Count {{ count.counter }}
     </p>
   </div>
 </template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { useStore } from '../store'
+
+export default defineComponent({
+  name: 'About',
+  setup () {
+    const store = useStore()
+    const count = ref(store.state)
+
+    return {
+      store,
+      count
+    }
+  }
+})
+</script>
