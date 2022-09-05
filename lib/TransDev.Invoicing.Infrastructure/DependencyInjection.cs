00449d90696d6357f41103a52d612b43f26ff63e
Patrick Lamm <lammster@gmail.com>
2022-09-04 22:15:14 -0400

    minror

--- lib/TransDev.Invoicing.Infrastructure/DependencyInjection.cs
+++ lib/TransDev.Invoicing.Infrastructure/DependencyInjection.cs
@@ -1,53 +1,48 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿namespace TransDev.Invoicing.Infrastructure;
+
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using TransDev.Invoicing.Application.Common.Interfaces;
 using TransDev.Invoicing.Infrastructure.Persistance;
 using TransDev.Invoicing.Infrastructure.Services;
 
-namespace TransDev.Invoicing.Infrastructure
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddInfrastructre(this IServiceCollection services, IConfiguration configuration)
     {
-        public static IServiceCollection AddInfrastructre(this IServiceCollection services, IConfiguration configuration)
+        if (configuration.GetValue<bool>("UseInMemoryDatabase") || configuration["inmemorydb"] == "true")
         {
-            if (configuration.GetValue<bool>("UseInMemoryDatabase") || configuration["inmemorydb"] == "true")
-            {
-                services.AddDbContext<ApplicationDbContext>(options =>
-                    options
-                        .UseLoggerFactory(MyLoggerFactory)
-                        .EnableSensitiveDataLogging(true)
-                        .UseInMemoryDatabase("cleanDb"));
-            }
-            else
-            {
-                services.AddDbContext<ApplicationDbContext>(options =>
-                    options
-                        .UseLoggerFactory(MyLoggerFactory)
-                        .EnableSensitiveDataLogging(true)
-                        .UseSqlServer(
-                        configuration.GetConnectionString("DefaultConnection"),
-                        b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
-            }
-
-            services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
-
-            services.AddTransient<IDateTime, DateTimeService>();
-            services.AddTransient<IItemService, ItemService>();
-            services.AddTransient<IAuthenticationService, AuthenticationService>();
-
-            return services;
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options
+                    .UseLoggerFactory(MyLoggerFactory)
+                    .EnableSensitiveDataLogging(true)
+                    .UseInMemoryDatabase("cleanDb"));
         }
-        public static readonly ILoggerFactory MyLoggerFactory = LoggerFactory.Create(builder =>
+        else
         {
-            builder.AddConsole();
-        });
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options
+                    .UseLoggerFactory(MyLoggerFactory)
+                    .EnableSensitiveDataLogging(true)
+                    .UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+        }
+
+        services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
+
+        services.AddTransient<IDateTime, DateTimeService>();
+        services.AddTransient<IItemService, ItemService>();
+        services.AddTransient<IAuthenticationService, AuthenticationService>();
+
+        return services;
     }
+
+    public static readonly ILoggerFactory MyLoggerFactory = LoggerFactory.Create(builder =>
+    {
+        builder.AddConsole();
+    });
 }
