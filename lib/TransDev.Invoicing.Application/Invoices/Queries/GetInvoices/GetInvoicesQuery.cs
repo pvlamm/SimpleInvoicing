661c581fc024b3cac9d6b4a2564a73463db9fb84
Patrick Lamm <lammster@gmail.com>
2023-05-28 18:32:11 -0400

    Added GetInvoicesQuery

---
+++ lib/TransDev.Invoicing.Application/Invoices/Queries/GetInvoices/GetInvoicesQuery.cs
@@ -0,0 +1,21 @@
+﻿namespace TransDev.Invoicing.Application.Invoices.Queries.GetInvoices;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using MediatR;
+
+using TransDev.Invoicing.Application.Common.Abstracts;
+
+public class GetInvoicesQuery : PaginationBase, IRequest<GetInvoicesQueryResult>
+{
+    public 
+}
+
+public class GetInvoicesQueryHandler : IRequestHandler<GetInvoicesQuery, GetInvoicesQueryResult>
+{
+    public Task<GetInvoicesQueryResult> Handle(GetInvoicesQuery request, CancellationToken cancellationToken)
+    {
+        throw new System.NotImplementedException();
+    }
+}
